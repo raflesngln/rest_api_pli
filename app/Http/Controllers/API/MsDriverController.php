856e1887a4210e0995f0e93a362e1b621599bc7a
do-dyco <dodhydnkz24@gmail.com>
2023-10-27 15:35:24 +0700

    link to collection

--- app/Http/Controllers/API/MsDriverController.php
+++ app/Http/Controllers/API/MsDriverController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\API;
+
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MsDriverResource;
 use App\Models\MsDriver;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -28,11 +30,13 @@ class MsDriverController extends Controller
 
         // Apply ordering
         $query = MsDriver::query();
-        $query->orderBy(isset($order_by)?$order_by:'id', isset($order_direction)?$order_direction:'asc');
-       // Apply pagination
-       $items = $query->paginate((int)$per_page, ['*'], 'page', (int)$page);
+        $query->orderBy(isset($order_by) ? $order_by : 'id', isset($order_direction) ? $order_direction : 'asc');
+        // Apply pagination
+        $items = $query->paginate((int)$per_page, ['*'], 'page', (int)$page);
 
-        return response()->json(['data'=>$items,'page'=>$page,'per_page'=>$per_page]);
+        $response = MsDriverResource::collection($items);
+
+        return response()->json(['data' => $response, 'page' => $page, 'per_page' => $per_page]);
     }
 
     public function show($id)
@@ -44,8 +48,8 @@ class MsDriverController extends Controller
         if (!$resp) {
             return response()->json(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
         }
-
-        return response()->json($resp);
+        $response = MsDriverResource::collection($resp);
+        return response()->json($response);
     }
 
     public function store(Request $request)
@@ -62,31 +66,29 @@ class MsDriverController extends Controller
 
         if ($validator->fails()) {
             echo $validator->messages()->toJson();
-        }else{
+        } else {
             $driver = MsDriver::create([
                 'driver_no' => $request['driver_no'],
                 'driver_name' => $request['driver_name'],
-                'driver_contact_number1' =>$request['driver_contact_number1'],
-                'driver_contact_number2' =>$request['driver_contact_number2'],
-                'is_active' =>$request['is_active'],
-                'is_deleted' =>$request['is_deleted'],
-                'ip' =>$request['ip'],
-                'create_by' =>$request['create_by'],
-                'vendor_id' =>$request['vendor_id'],
-                'driver_email' =>$request['driver_email'],
-                'driver_pass' =>bcrypt($request['driver_pass'])
+                'driver_contact_number1' => $request['driver_contact_number1'],
+                'driver_contact_number2' => $request['driver_contact_number2'],
+                'is_active' => $request['is_active'],
+                'is_deleted' => $request['is_deleted'],
+                'ip' => $request['ip'],
+                'create_by' => $request['create_by'],
+                'vendor_id' => $request['vendor_id'],
+                'driver_email' => $request['driver_email'],
+                'driver_pass' => bcrypt($request['driver_pass'])
             ]);
 
 
             $response = [
                 'driver' => $driver,
-                'message' =>'Success create data'
+                'message' => 'Success create data'
             ];
 
             return response($response, 201);
         }
-
-
     }
 
     public function update(Request $request, $id)
@@ -114,5 +116,4 @@ class MsDriverController extends Controller
         $resp->delete();
         return response()->json(['message' => 'User deleted']);
     }
-
 }
