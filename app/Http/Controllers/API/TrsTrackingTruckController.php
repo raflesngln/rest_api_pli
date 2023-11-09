0607de1b377bc67bbc823aafb484d0b23e74c991
Rafles Nainggolan <raflesngln@gmail.com>
2023-11-09 15:29:07 +0700

    obs updates1

--- app/Http/Controllers/API/TrsTrackingTruckController.php
+++ app/Http/Controllers/API/TrsTrackingTruckController.php
@@ -58,7 +58,7 @@ class TrsTrackingTruckController extends Controller
         $attachment = $request['attachment'];
         $attachment = base64_decode($attachment);
 
-        $filename= $id_dispatch.'_'.date('Y-m-d H:i:s');
+        $filename= $id_dispatch.'_'.date('Y-m-d-H-i-s');
         // $fileContents = file_get_contents($attachment);
 
         $validator = Validator::make($request->all(), [
